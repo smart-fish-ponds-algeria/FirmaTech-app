5540e8d19107f7ef50a7a0fc2c3a022af13606b3
YounesBensafia <younes.bensafia@etu.usthb.dz>
2025-07-18 08:13:00 +0100

    feat: implement dashboard screen with water tank details and loading/error handling

--- app/index.tsx
+++ app/index.tsx
@@ -1,28 +1,41 @@
-import React, { useState } from 'react';
-import { View, TextInput, Button, Text } from 'react-native';
-import { login } from '../services/authService'; // adapte le chemin si besoin
+import { router } from "expo-router";
+import React, { useState } from "react";
+import { Button, Text, TextInput, View } from "react-native";
+import { login } from "../services/authService"; // adapte le chemin si besoin
 
 export default function LoginScreen() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [error, setError] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
 
   const handleLogin = async () => {
     try {
       const data = await login(email, password);
-      console.log('Login success:', data);
-      // tu peux stocker le token avec SecureStore ou AsyncStorage
+      console.log("Login successful:", data);
+      const userId = data.data._id;
+      if (!userId) {
+        setError("Aucun ID d'utilisateur trouvé dans la réponse.");
+        return;
+      }
+      console.log("Redirecting to /dashboard...");
+      console.log("User ID:", userId);
+      router.replace(`/dashboard?userId=${userId}`);
     } catch (err: any) {
-      setError(err?.response?.data?.message || 'Login failed');
+      setError(err?.response?.data?.message || "Login failed");
     }
   };
 
   return (
     <View style={{ padding: 20 }}>
       <TextInput placeholder="Email" value={email} onChangeText={setEmail} />
-      <TextInput placeholder="Mot de passe" value={password} onChangeText={setPassword} secureTextEntry />
+      <TextInput
+        placeholder="Mot de passe"
+        value={password}
+        onChangeText={setPassword}
+        secureTextEntry
+      />
       <Button title="Connexion" onPress={handleLogin} />
-      {error ? <Text style={{ color: 'red' }}>{error}</Text> : null}
+      {error ? <Text style={{ color: "red" }}>{error}</Text> : null}
     </View>
   );
 }
